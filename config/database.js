5f5de89c36ae90c7778d116bb82f94b26510ea8f
unknown <jithinolickal@gmail.com>
2021-05-07 00:54:57 +0530

    changed to connection pool

--- config/database.js
+++ config/database.js
@@ -8,11 +8,30 @@ if (process.env.NODE_ENV !== 'production') {
 
 console.log("database.js", process.env.MYSQL_DATABASE);
 
-const db = mysql.createConnection({
+const dbConnectionInfo  = {
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE
-})
+}
+
+//create mysql connection pool
+var db = mysql.createPool(
+    dbConnectionInfo
+  );
+  
+  // Attempt to catch disconnects 
+  db.on('connection', function (connection) {
+    console.log('DB Connection established');
+  
+    connection.on('error', function (err) {
+      console.error(new Date(), 'MySQL error', err.code);
+    });
+    connection.on('close', function (err) {
+      console.error(new Date(), 'MySQL close', err);
+    });
+  
+  });
+  
 
 module.exports = db
